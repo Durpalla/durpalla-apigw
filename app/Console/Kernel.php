59646da83992d4cf044a7e981e30037f0b03935d
Jewel Rana <jewelrana.dev@gmail.com>
2025-08-22 14:35:29 +0600

    Fix

---
+++ app/Console/Kernel.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
+
+class Kernel extends ConsoleKernel
+{
+    protected function schedule(Schedule $schedule): void
+    {
+        $schedule->command('stoppage:update')
+            ->daily()
+//            ->environments(['production'])
+            ->runInBackground()
+            ->after(function() {
+                Log::debug('Updated stoppages');
+            });
+
+        $schedule->command('release:lock')
+            ->everyFiveMinutes()
+            ->runInBackground()
+            ->before(function () {
+                Log::debug('Releasing lock items');
+            })
+            ->after(function () {
+                Log::debug('Lock items released');
+            });
+
+        $schedule->command('trip:update')
+            ->everyMinute();
+//            ->environments(['staging', 'production']);
+
+        $schedule->command('booking:pending')
+            ->everyFifteenMinutes()
+//            ->environments(['staging', 'production'])
+            ->runInBackground()
+            ->before(function () {
+                Log::debug('Handling pending bookings');
+            })
+            ->after(function () {
+                Log::debug('Pending booking handled');
+            });
+    }
+
+    protected function commands(): void
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
